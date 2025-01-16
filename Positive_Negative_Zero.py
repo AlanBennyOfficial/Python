135faaadc19ab267457a72055c3e9e504d4b25ee
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-01-16 20:26:10 +0300

    Create Positive_Negative_Zero.py

---
+++ Positive_Negative_Zero.py
@@ -0,0 +1,10 @@
+# Check weather the given number is positive, negative or zero
+
+n = int(input("Enter a number:"))
+
+if n<0:
+  print("It is negative")
+elif n>0:
+  print("It is positive")
+else:
+  print("It is zero")
