242f23ac7d6c5b7076719505d6bb4b4a2f402069
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-01-20 20:02:25 +0300

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -7,3 +7,20 @@ if (sympy.isprime(n)):
   print("It is prime")
 else:
   print("It is not prime")
+
+# Another method
+
+'''
+def is_prime(num):
+  for x in range( 2, num):
+    if (num%x) == 0:
+      return False
+  return True
+
+x = int(input("Enter the number:"))
+if( is_prime(x)):
+  print("It is prime")
+else:
+  print("It is not prime")
+  
+'''
