b174569da1ff55e02603439134c69fae46a9c065
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-05-14 00:22:33 +0530

    Added files via git

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,14 @@
 n=int(input("Enter a number:"))
 fact = 1
 for i in range(1,n+1):
-        fact *= i
+    fact *= i
 
 print(f"The factorial of {n} is {fact}")
+
+'''
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+'''
---
+++ Practice/Triangle_Area.py
@@ -0,0 +1,7 @@
+# Find the Base and Height of a triangle and find the area of it.
+
+# Area of triangle = 1/2 * base * height
+def area_of_triangle(base, height):
+    return 0.5 * base * height
+
+print(area_of_triangle(5,4))
